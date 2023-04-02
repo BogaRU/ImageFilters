6572bcb7fd1ae0800f36fe603eed9ea83b350a39
Богдан Ведерников <botom123456@mail.ru>
2023-04-02 14:32:43 +0300

    Добавьте файлы проекта.

---
+++ GrayscaleTask.cs
@@ -0,0 +1,21 @@
+﻿namespace Recognizer
+{
+	public static class GrayscaleTask
+	{
+		 // Яркость = (0.299*R + 0.587*G + 0.114*B) / 255
+		public static double[,] ToGrayscale(Pixel[,] original)
+		{
+			int xLength = original.GetLength(0);
+			int yLength = original.GetLength(1);
+			double[,] result = new double[xLength, yLength];
+			for (int i = 0; i < xLength; i++)
+			{
+				for (int j = 0; j < yLength; j++)
+				{
+					result[i, j] = (0.299 * original[i, j].R + 0.587 * original[i, j].G + 0.114 * original[i, j].B) / 255;
+                }
+			}
+			return result;
+		}
+	}
+}
---
+++ ThresholdFilterTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Recognizer
+{
+	public static class ThresholdFilterTask
+	{
+		public static double[,] ThresholdFilter(double[,] original, double whitePixelsFraction)
+		{
+			int rows = original.GetLength(0);
+			int columns = original.GetLength(1);
+            int whitePixelsCount = (int)Math.Floor(original.Length * whitePixelsFraction);
+			if (original.Length == 1) return whitePixelsCount > 0 ? new double[,] { { 1.0 } } : new double[,] { { 0.0 } };
+			double[,] result = new double[rows, columns];
+            List<double> sortedOriginal = new List<double>();
+            foreach (var x in original)
+                sortedOriginal.Add(x);
+            sortedOriginal.Sort();
+			int offset = (whitePixelsCount == 0) ? 1 : 0;
+			double T = sortedOriginal[sortedOriginal.Count - Math.Max(whitePixelsCount, 1)]
+				+ offset * sortedOriginal[sortedOriginal.Count - Math.Max(whitePixelsCount, 1)] / 100;
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < columns; j++)
+				{
+					if (original[i, j] >= T) result[i, j] = 1.0;
+					else result[i, j] = 0.0;
+				} 
+			}
+			return result;
+		}
+	}
+}
